f3b00969720b9ba825c56c7eb2b9ee2b4225e75a
beatrice604 <beatrice.schuller@ulbsibiu.ro>
2022-12-27 19:11:21 +0200

    Created header, homePage

---
+++ src/main/webapp/resources/js/header.js
@@ -0,0 +1,11 @@
+/*const button = document.getElementById('myButton');
+const container = document.getElementById('hamby');
+const menu=document.getElementById("hamburger_menu");
+const btn=document.getElementById("hamburger_button");
+
+// Add an event listener to the button to handle clicks
+
+button.addEventListener('click', function () {
+    console.log("works");
+    menu.classList.add("collapse");
+});*/
